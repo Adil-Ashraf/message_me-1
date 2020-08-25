a6ee03faf9307a1e93945a5c59c7e587a3888d54
Adil-Ashraf <adil.ashraf@devbox.co>
2020-08-25 12:02:54 +0500

    changes maded 1st time

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+changes made by mee The Adil 12
--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,7 +3,7 @@ class ChatroomController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = Message.custom_display
+    @messages = Message.custom_display #comment it out
   end
 
 end
